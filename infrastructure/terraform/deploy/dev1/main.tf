de1cb825a93e58e629e55de2e703bda9b54174dd
Naren Kumar <naren1340@gmail.com>
2026-03-30 00:26:10 -0700

    updated vpc module

--- infrastructure/terraform/deploy/dev1/main.tf
+++ infrastructure/terraform/deploy/dev1/main.tf
@@ -1,21 +1,13 @@
-terraform {
-  required_version = ">= 1.11"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.region
-}
-
 module "vpc" {
   source = "../../modules/vpc"
 
   project_name = var.project_name
+}
 
+module "eks" {
+  source = "../../modules/eks"
+
+  eks_cluster_name = vars.eks_cluster_name
+  vpc_id           = module.vpc.vpc_id
+  public_subnet_ids = module.vpc.public_subnet_ids
 }
--- infrastructure/terraform/deploy/dev1/terraform.tfvars
+++ infrastructure/terraform/deploy/dev1/terraform.tfvars
@@ -1,2 +1,2 @@
-region          = "us-east-1"
 project_name    = "nt"
+region          = "us-east-1"
